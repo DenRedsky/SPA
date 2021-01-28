89a08be2081e043f86bcb614b42770fd44bad5c4
Denis Krasnovskiy <DIKrasnovskii@domclick.ru>
2021-01-28 14:25:59 +0300

    public path

--- webpack.prod.js
+++ webpack.prod.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     filename: 'js/[name].[contenthash].js',
-    publicPath
+    publicPath: 'https://denredsky.github.io/webpack-react-redux-saga-router-stylus/'
   },
   resolve: {
     alias: {

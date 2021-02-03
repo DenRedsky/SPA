61aded613e0010fb9b232f139c3e2f4c7babcde7
Denis Krasnovskiy <DIKrasnovskii@domclick.ru>
2021-02-03 15:52:54 +0300

    tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,26 +1,14 @@
 exports.config = {
-  runner: 'local',
   specs: [
     './test/specs/**/*.js'
   ],
   capabilities: [
-    {
-      browserName: 'opera'
-
-    }
+    { browserName: 'chrome' },
+    { browserName: 'firefox' },
+    { browserName: 'safari' }
   ],
   logLevel: 'error',
-  bail: 0,
   baseUrl: 'http://localhost',
-  waitforTimeout: 10000,
-  connectionRetryTimeout: 120000,
-  connectionRetryCount: 3,
-  framework: 'mocha',
   reporters: ['spec'],
-  services: ['selenium-standalone'],
-  mochaOpts: {
-    require: ['@babel/register'],
-    ui: 'bdd',
-    timeout: 60000
-  }
+  services: ['selenium-standalone']
 };
